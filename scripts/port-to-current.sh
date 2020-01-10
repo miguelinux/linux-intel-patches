c23af44832a92d1837293ee779c53eadc21e45de
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-01-09 18:14:50 -0600

    scripts: improve a little bit

--- scripts/port-to-current.sh
+++ scripts/port-to-current.sh
@@ -9,7 +9,7 @@ for p in $*
 do
     if ! git am --quiet $p 2> /dev/null
     then
-        rm -rf ${rejf}
+        rm -f ${rejf}
         if ! patch --quiet --reject-file=${rejf} --forward -p1 < $p
         then
             if [ -f ${rejf} ]
@@ -22,10 +22,17 @@ do
                 fi
             fi
         fi
-        echo sleep 3
-        sleep 3
-        git add --all
-        git am --quiet --continue
+        git status
+        echo $p
+        read dopause
+        if git diff --no-ext-diff --quiet
+        then
+            git am --quiet --skip
+            git -C ${p%/*} rm --quiet ${p#*/}
+        else
+            git add --all
+            git am --quiet --continue
+        fi
     fi
 done
 rm -rf ${tmpdir}
